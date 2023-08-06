e44b5ec8bda7a0581495bff4e9e225b2da1ca683
Matthias Hadlich <matthiashadlich@posteo.de>
2023-08-06 10:00:16 +0000

    Add caching

--- sample-apps/radiology/lib/configs/sw_interactive_segmentation.py
+++ sample-apps/radiology/lib/configs/sw_interactive_segmentation.py
@@ -63,6 +63,7 @@ class SWInteractiveSegmentationConfig(TaskConfig):
             labels=self.labels,
             label_names=self.label_names, 
             preload=strtobool(self.conf.get("preload", "false")),
+            config={"cache_transforms": True, "cache_transforms_in_memory": True, "cache_transforms_ttl": 300},
         )
         return task
 
